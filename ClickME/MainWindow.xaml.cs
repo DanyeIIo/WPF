ab1c4e5dcd14658694eaa4c2b5feb79fa5b3e44f
Danyello <ulkiora.panda@gmail.com>
2021-06-08 20:38:20 +0600

    useless clicker

---
+++ ClickME/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClickME
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private readonly Random rand = new Random();
+
+        private void buttonToClick_Click(object sender, RoutedEventArgs e)
+        {
+            var textPercent = rand.Next(1, 71);
+            var buttonSizePercent = rand.Next(0, 26);
+            var windowSizePercent = rand.Next(0, 6);
+
+            buttonToClick.Content = Randomizer.Randomize(buttonToClick.Content.ToString(), textPercent);
+            buttonToClick.Width += buttonToClick.Width * buttonSizePercent / 100;
+            buttonToClick.Height += buttonToClick.Height * buttonSizePercent / 100;
+
+            var window = Application.Current.MainWindow;
+
+            Application.Current.MainWindow.Height += window.Height * windowSizePercent / 100;
+            Application.Current.MainWindow.Width += window.Width * windowSizePercent / 100;
+        }
+    }
+}
---
+++ ClickME/Randomizer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickME
+{
+    /// <summary>
+    /// Shuffle collections and string works like a library
+    /// </summary>
+    public class Randomizer
+    {
+        private Randomizer() { }
+
+        /// <summary>
+        /// method to shuffle collections
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items">Collection (taking shuffled collection)</param>
+        /// <param name="percentage">Percent to shuffle</param>
+        public static void Randomize<T>(ref T[] items, int percentage)
+        {
+            if (items is null || items.Length == 0)
+            {
+                throw new ArgumentNullException($"{nameof(items)} can not be null or can't be contains empty", nameof(items));
+            }
+
+            if (percentage <= 0 || percentage > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percentage));
+            }
+
+            Random rand = new Random();
+
+            for (uint i = 0; i < percentage; i++)
+            {
+                int left = rand.Next(0, items.Length);
+                int right = rand.Next(0, items.Length);
+
+                (items[left], items[right]) = (items[right], items[left]);
+            }
+        }
+
+        /// <summary>
+        /// method to string shuffle
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="percentage">Percent to shuffle</param>
+        /// <returns>shuffled string result</returns>
+        public static string Randomize(string items, int percentage)
+        {
+            if (string.IsNullOrEmpty(items))
+            {
+                throw new ArgumentException($"'{nameof(items)}' cannot be null or empty.", nameof(items));
+            }
+
+            if (percentage <= 0 || percentage > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percentage));
+            }
+
+            Random rand = new Random();
+
+            var res = items.ToCharArray();
+
+            for (uint i = 0; i < percentage; i++)
+            {
+                int left = rand.Next(0, res.Length);
+                int right = rand.Next(res.Length / 2, res.Length);
+
+                (res[left], res[right]) = (res[right], res[left]);
+            }
+
+            return new string(res);
+        }
+    }
+}
