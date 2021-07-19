370ca22d64943ed81dfd3d9b203d5169d9e9d5ca
Danyello <ulkiora.panda@gmail.com>
2021-07-19 15:48:48 +0600

    multiple windows

---
+++ Windoww/NewWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Windoww
+{
+    /// <summary>
+    /// Interaction logic for NewWindow.xaml
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            //var w = new NewWindow();
+            var win = new WindowsFolder();
+
+            win.Show();
+            //w.Show();
+        }
+
+        private void Magic_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+
+            foreach (Window window in App.Current.Windows)
+            {
+                window.Title = "Hi Yo";
+                window.Height = rand.Next(100,501);
+                window.Background = new SolidColorBrush(
+                    Color.FromArgb(
+                        (byte)rand.Next(256),
+                        (byte)rand.Next(256),
+                        (byte)rand.Next(256),
+                        (byte)rand.Next(256)));
+            }
+        }
+    }
+}
---
+++ Windoww/WindowsFolder.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Windoww
+{
+    /// <summary>
+    /// Interaction logic for WindowsFolder.xaml
+    /// </summary>
+    public partial class WindowsFolder : Window
+    {
+        public WindowsFolder()
+        {
+            InitializeComponent();
+        }
+
+        string nameOfFolder = @"C:\Windows";
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var file in Directory.EnumerateDirectories(nameOfFolder))
+            {
+                TreeViewItem viewItem = new TreeViewItem();
+                viewItem.Header = file.Substring(file.LastIndexOf('\\') + 1);
+                viewItem.Tag = file;
+                FillTreeViews(viewItem, file);
+                treeViewItem.Items.Add(viewItem);
+            }
+        }
+        private void FillTreeViews(TreeViewItem parentItem, string path)
+        {
+            try
+            {
+                foreach (var str in Directory.EnumerateDirectories(path))
+                {
+                    TreeViewItem item = new TreeViewItem();
+                    item.Header = str.Substring(str.LastIndexOf('\\') + 1);
+                    item.Tag = str;
+                    item.FontWeight = FontWeights.Normal;
+                    parentItem.Items.Add(item);
+                    FillTreeViews(item, str);
+                }
+            }
+            catch { }
+        }
+    }
+}
