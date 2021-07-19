2a51a409e20371e09df1f0f8392719b76307452f
Danyello <ulkiora.panda@gmail.com>
2021-07-19 15:53:51 +0600

    Controls learn

--- Controls2/MainWindow.xaml.cs
+++ Controls2/MainWindow.xaml.cs
@@ -26,61 +26,61 @@ namespace Controls2
             InitializeComponent();
         }
 
-        //        private void B1_Click(object sender, RoutedEventArgs e)
-        //        {
-        //            MessageBox.Show(pb1.Password);
-        //            //while (pb.Value < 100)
-        //            //{
-        //            //    pb.Value++;
-        //            //    Thread.Sleep(200);
-        //            //}
-        //        }
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(pb1.Password);
+            //while (pb.Value < 100)
+            //{
+            //    pb.Value++;
+            //    Thread.Sleep(200);
+            //}
+        }
 
-        //        private void pb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        //        {
+        private void pb_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
 
-        //        }
+        }
         private void Win_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("You won!");
         }
         Random random = new Random();
-        private void MouseMove(object sender, MouseEventArgs e)
-        {
+        //private void MouseMove(object sender, MouseEventArgs e)
+        //{
 
-            double x = Canvas.GetLeft(bMain);
-            double y = Canvas.GetTop(bMain);
+        //    double x = Canvas.GetLeft(bMain);
+        //    double y = Canvas.GetTop(bMain);
 
 
 
-            int side = random.Next(1, 5);
-            switch (side)
-            {
-                case 1:
-                    x += bMain.Width;
-                    break;
-                case 2:
-                    x -= bMain.Width;
-                    break;
-                case 3:
-                    y += bMain.Height;
-                    break;
-                case 4:
-                    y -= bMain.Height;
-                    break;
-                default:
-                    break;
-            }
-            Title = $"side={side}; x={x}; y={y}; ";
-            Canvas.SetLeft(bMain, x);
-            Canvas.SetTop(bMain, y);
+        //    int side = random.Next(1, 5);
+        //    switch (side)
+        //    {
+        //        case 1:
+        //            x += bMain.Width;
+        //            break;
+        //        case 2:
+        //            x -= bMain.Width;
+        //            break;
+        //        case 3:
+        //            y += bMain.Height;
+        //            break;
+        //        case 4:
+        //            y -= bMain.Height;
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //    Title = $"side={side}; x={x}; y={y}; ";
+        //    Canvas.SetLeft(bMain, x);
+        //    Canvas.SetTop(bMain, y);
 
-            if ((x + bMain.Width + 20 > Width) || (y + bMain.Height + 50 > Height) || (y < 0) || (x < 0))
-            {
-                Canvas.SetLeft(bMain, 0);
-                Canvas.SetTop(bMain, 0);
-                MouseMove(null, null);
-            }
-        }
+        //    if ((x + bMain.Width + 20 > Width) || (y + bMain.Height + 50 > Height) || (y < 0) || (x < 0))
+        //    {
+        //        Canvas.SetLeft(bMain, 0);
+        //        Canvas.SetTop(bMain, 0);
+        //        MouseMove(null, null);
+        //    }
+        //}
     }
 }
